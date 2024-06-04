53d5e6273d4fc0488bddecf144776f438a7d07da
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-06-05 01:45:58 +0700

    second init

---
+++ app/src/main/java/com/example/iot_project/AlarmReceiver.java
@@ -0,0 +1,69 @@
+package com.example.iot_project;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Date;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final String CHANNEL_ID = "201";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if ("hengio".equals(intent.getAction())) {
+            String timePicker = intent.getStringExtra("timePicker");
+            int area = intent.getIntExtra("area", 0);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (notificationManager != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel 1", NotificationManager.IMPORTANCE_HIGH);
+                    channel.setDescription("Mieu ta kenh 1");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setContentTitle(timePicker)
+                        .setContentText("Khu vực " + area + " bắt đầu tưới")
+                        .setColor(Color.RED)
+                        .setSmallIcon(R.drawable.icon_notifications)
+                        .setCategory(NotificationCompat.CATEGORY_ALARM)
+                        .setDefaults(NotificationCompat.DEFAULT_SOUND);
+
+                notificationManager.notify(getNotificationId(), builder.build());
+
+            }
+        } else if ("ketthuc".equals(intent.getAction())) {
+            int area = intent.getIntExtra("area", 0);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (notificationManager != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel 2", NotificationManager.IMPORTANCE_HIGH);
+                    channel.setDescription("Mieu ta kenh 2");
+                    notificationManager.createNotificationChannel(channel);
+                }
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setContentTitle("Khu vực " +area +" kết thúc tưới")
+                        .setColor(Color.RED)
+                        .setSmallIcon(R.drawable.icon_notifications)
+                        .setCategory(NotificationCompat.CATEGORY_ALARM)
+                        .setDefaults(NotificationCompat.DEFAULT_SOUND);
+
+                notificationManager.notify(getNotificationId(), builder.build());
+            }
+        }
+    }
+
+    private int getNotificationId() {
+        return (int) (new Date().getTime() % Integer.MAX_VALUE);
+    }
+}
--- app/src/main/java/com/example/iot_project/WaterFragment.java
+++ app/src/main/java/com/example/iot_project/WaterFragment.java
@@ -1,23 +1,35 @@
 package com.example.iot_project;
 
+import static android.content.Context.ALARM_SERVICE;
+
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class WaterFragment extends Fragment {
 
-    private LinearLayout layout_btn_on, layout_tu_dong;
-    private Button btn_thu_cong, btn_tu_dong, btnTuoi;
-    private boolean chedo = false;
-    private String gui = "testcode";
+    LinearLayout layout_btn_on, layout_tu_dong,layout_btn_off;
+    Button btn_thu_cong, btn_tu_dong, btnTuoi,btnTat;
+    EditText water,timePicker,water_time;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private int chedo = 0;
+    private int waterInt=0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,18 +40,23 @@ public class WaterFragment extends Fragment {
         CheckBox area3 = view.findViewById(R.id.area3);
         layout_btn_on = view.findViewById(R.id.layout_btn_on);
         layout_tu_dong = view.findViewById(R.id.layout_tu_dong);
+        layout_btn_off=view.findViewById(R.id.layout_btn_off);
         btn_thu_cong = view.findViewById(R.id.btn_thu_cong);
         btn_tu_dong = view.findViewById(R.id.btn_tu_dong);
         btnTuoi = view.findViewById(R.id.btn_tuoi);
+        btnTat=view.findViewById(R.id.btn_tat);
+        water=view.findViewById(R.id.water);
         layout_btn_on.setVisibility(View.GONE);
+        layout_btn_off.setVisibility(View.GONE);
         layout_tu_dong.setVisibility(View.GONE);
 
         btn_thu_cong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 layout_btn_on.setVisibility(View.VISIBLE);
-                layout_tu_dong.setVisibility(View.VISIBLE);
-                chedo = true;
+                layout_btn_off.setVisibility(View.VISIBLE);
+                layout_tu_dong.setVisibility(View.GONE);
+                chedo = 1;
             }
         });
 
@@ -47,20 +64,125 @@ public class WaterFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 layout_btn_on.setVisibility(View.VISIBLE);
-                layout_tu_dong.setVisibility(View.GONE);
-                chedo = true;
+                layout_btn_off.setVisibility(View.GONE);
+                layout_tu_dong.setVisibility(View.VISIBLE);
+                chedo = 2;
             }
         });
 
         btnTuoi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!area1.isChecked() && !area2.isChecked() && !area3.isChecked()) {
-                    showAlert("Bạn chưa chọn khu vực");
-                } else {
-                    if (!chedo) {
-                        showAlert("Bạn chưa chọn chế độ");
-                    } else {
+                if (!area1.isChecked() && !area2.isChecked() && !area3.isChecked()) {showAlert("Bạn chưa chọn khu vực");}
+                else {
+                    if ((chedo!=1)&&(chedo!=2)) {showAlert("Bạn chưa chọn chế độ");}
+                    else
+                    {
+                        String waterText = water.getText().toString();
+                        if (waterText.isEmpty()) {showAlert("Bạn chưa nhập đủ thông tin");}
+                        else
+                        {
+                            waterInt = Integer.parseInt(waterText);
+                            if (waterInt<=0) {showAlert("Lượng nước không hợp lệ. Vui lòng nhập lại.");}
+                            else
+                            {
+                                int khuvuc = 0;
+                                if (area1.isChecked()) {
+                                    khuvuc = 1;
+                                } else if (area2.isChecked()) {
+                                    khuvuc = 2;
+                                } else if (area3.isChecked()) {
+                                    khuvuc = 3;
+                                }
+                                if(chedo==1)
+                                {
+                                    Toast.makeText(getActivity(), "Khu vực " + khuvuc + " tuới thành công !", Toast.LENGTH_SHORT).show();
+                                    water.setText("");
+                                    int finalKhuvuc = khuvuc;
+                                    btnTat.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Toast.makeText(getActivity(), "Khu vực " + finalKhuvuc + " tắt thành công !", Toast.LENGTH_SHORT).show();
+                                            water.setText("");
+                                        }
+                                    });
+                                }
+                                else {
+                                    int hour=0,minute=0,water_timeInt=0;
+                                    timePicker = view.findViewById(R.id.timePicker);
+                                    water_time= view.findViewById(R.id.water_time);
+                                    String timeText = timePicker.getText().toString();
+                                    String water_timeText = water_time.getText().toString();
+                                    water_timeInt = Integer.parseInt(water_timeText);
+                                    if (timeText.isEmpty() || water_timeText.isEmpty()) {showAlert("Bạn chưa nhập đủ thông tin");}
+                                    else {
+                                        if (water_timeInt<1) {showAlert("Số phút tưới không hợp lệ. Vui lòng nhập lại.");}
+                                        else
+                                        {
+                                            if (timeText.contains(":")) {
+                                                String[] timeParts = timeText.split(":");
+                                                hour = Integer.parseInt(timeParts[0].trim());
+                                                minute = Integer.parseInt(timeParts[1].trim());
+                                                if ((hour < 0 || hour > 23) || (minute < 0 || minute > 59))
+                                                {
+                                                    showAlert("Thời gian không hợp lệ. Vui lòng nhập lại.");
+                                                    return;
+                                                }
+
+                                                Calendar calendar = Calendar.getInstance();
+                                                calendar.setTimeInMillis(System.currentTimeMillis());
+                                                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                                                calendar.set(Calendar.MINUTE, minute);
+                                                calendar.set(Calendar.SECOND, 0);
+
+                                                String shour = String.valueOf(hour);
+                                                String sminute = String.valueOf(minute);
+                                                if (minute < 10) {
+                                                    sminute = "0" + minute;
+                                                }
+                                                Intent intent = new Intent(getContext(), AlarmReceiver.class);
+                                                intent.setAction("hengio");
+                                                intent.putExtra("area", khuvuc);
+                                                intent.putExtra("timePicker", shour + ":" + sminute);
+                                                alarmManager = (AlarmManager) getContext().getSystemService(ALARM_SERVICE);
+                                                pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                                                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                                                Toast.makeText(getActivity(), "Đặt lịch tuới khu vực " + khuvuc + " thành công !", Toast.LENGTH_SHORT).show();
+
+                                                int water_timeIntCD=minute+water_timeInt;
+                                                if(water_timeIntCD>=60)
+                                                {
+                                                    hour=hour+water_timeIntCD/60;
+                                                    minute=water_timeIntCD%60;
+                                                }
+                                                else
+                                                {
+                                                    minute=water_timeIntCD;
+                                                }
+
+                                                calendar.set(Calendar.HOUR_OF_DAY, hour);
+                                                calendar.set(Calendar.MINUTE, minute);
+                                                calendar.set(Calendar.SECOND, 0);
+
+                                                intent.setAction("ketthuc");
+                                                intent.putExtra("area", khuvuc);
+                                                alarmManager = (AlarmManager) getContext().getSystemService(ALARM_SERVICE);
+                                                pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                                                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                                                water.setText("");
+                                                timePicker.setText("");
+                                                water_time.setText("");
+
+                                            }
+                                            else {
+                                                showAlert("Định dạng thời gian không hợp lệ. Vui lòng nhập lại.");
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }//
 
                     }
                 }
