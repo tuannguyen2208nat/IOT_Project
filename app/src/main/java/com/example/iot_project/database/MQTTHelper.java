8d8d1b4bbb1a09ecd24eca3311b86ac56f61e50e
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-06-09 00:15:46 +0700

    Thirteenth commit

--- app/src/main/java/com/example/iot_project/database/MQTTHelper.java
+++ app/src/main/java/com/example/iot_project/database/MQTTHelper.java
@@ -1,5 +1,7 @@
 package com.example.iot_project.database;
+
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.util.Log;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -12,19 +14,20 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class MQTTHelper {
     public MqttAndroidClient mqttAndroidClient;
-
-    public final String[] arrayTopics = {"tien2032002/feeds/temp", "tien2032002/feeds/humid", "tien2032002/feeds/light"};
-
+    private String username;
+    private String password;
+    private String serverUri;
+    public String[] arrayTopics;
     final String clientId = "12345678";
-    final String username = "tien2032002";
-    final String password = "aio_GXvU69CJJL9x57EjgNAPi11K2e7l";
-
-    final String serverUri = "tcp://io.adafruit.com:1883";
 
     public MQTTHelper(Context context){
+        loadCredentials(context);
         mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
         mqttAndroidClient.setCallback(new MqttCallbackExtended() {
             @Override
@@ -50,6 +53,24 @@ public class MQTTHelper {
         connect();
     }
 
+    private void loadCredentials(Context context) {
+        AssetManager assetManager = context.getAssets();
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = assetManager.open("mqtt_config.properties");
+            properties.load(inputStream);
+            username = properties.getProperty("username");
+            password = properties.getProperty("password");
+            serverUri = properties.getProperty("serverUri", "tcp://io.adafruit.com:1883");
+            inputStream.close();
+
+            // Initialize arrayTopics after loading the username
+            arrayTopics = new String[] { username + "/feeds/temp", username + "/feeds/humid", username + "/feeds/light" };
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void setCallback(MqttCallbackExtended callback) {
         mqttAndroidClient.setCallback(callback);
     }
@@ -62,11 +83,9 @@ public class MQTTHelper {
         mqttConnectOptions.setPassword(password.toCharArray());
 
         try {
-
             mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-
                     DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
                     disconnectedBufferOptions.setBufferEnabled(true);
                     disconnectedBufferOptions.setBufferSize(100);
@@ -81,31 +100,29 @@ public class MQTTHelper {
                     Log.w("Mqtt", "Failed to connect to: " + serverUri + exception.toString());
                 }
             });
-
-
         } catch (MqttException ex){
             ex.printStackTrace();
         }
     }
 
     private void subscribeToTopic() {
-        for(int i = 0; i < arrayTopics.length; i++) {
+        for (String topic : arrayTopics) {
             try {
-                mqttAndroidClient.subscribe(arrayTopics[i], 0, null, new IMqttActionListener() {
+                mqttAndroidClient.subscribe(topic, 0, null, new IMqttActionListener() {
                     @Override
                     public void onSuccess(IMqttToken asyncActionToken) {
                         Log.d("TEST", "Subscribed!");
                     }
+
                     @Override
                     public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                         Log.d("TEST", "Subscribed fail!");
                     }
                 });
             } catch (MqttException ex) {
-                System.err.println("Exceptionst subscribing");
+                System.err.println("Exception subscribing");
                 ex.printStackTrace();
             }
         }
     }
-
 }
